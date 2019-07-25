b469ed5fee0acdb5ac04c7e415ccfbc619c0bd8c
Nikita Litvin <n@deltaidea.com>
2019-07-25 10:42:02 +0500

    chore(vagrant): get rid of Vagrant files

--- watch.sh
+++
@@ -1,4 +0,0 @@
-while inotifywait --recursive -e modify -e delete -e create -e move /home/vagrant/overviewer-config; do
-	sudo overviewer.py --config=/vagrant/test-config.py --genpoi
-	sudo cp -r /vagrant/icons/* /opt/minecraft-map/icons
-done
